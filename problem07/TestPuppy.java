a5e052ce35923c8a5310cdf61f9c1b07e3e60fec
marigichun <34428772+marigichun@users.noreply.github.com>
2017-12-14 19:31:28 +0900

    Puppy 비교후 출력하는 법

---
+++ problem07/TestPuppy.java
@@ -0,0 +1,21 @@
+public class TestPuppy {
+
+	public static void main(String[] args) {
+
+
+	Person puppy = new Puppy ("Ω√√Ú", "∂∫¿Ã", 2, "ºˆƒ∆");
+	Person ppoppy= new Puupy ("Ω√√Ú","∂∫¿Ã", 3, "æœƒ∆");
+
+	boolean isEqual = puppy.equals (ppoppy);
+	System.out.println(isEquals);
+
+
+	boolean isEqual02 = ppoppy.equals(puppy);
+	System.out.println(isEquals02);
+
+	if (isEquals) {
+
+		} 
+	}
+
+}
